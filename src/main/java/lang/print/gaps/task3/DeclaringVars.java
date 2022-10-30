0371afb2a72eb920c3a01411359383682f362958
wilkat <katarzyna.f.wilk@gmail.com>
2022-10-30 23:10:39 +0100

    New

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,7 +2,8 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c = 10;
+        int a, b, c;
+         a = b = c = 10;
         System.out.println(c);
     }
 }
